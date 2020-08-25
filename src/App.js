bec8af00c91fa19ceb36635bdaaefbc2ef9799a0
Harrison Aguiar <harryaguiar@gmail.com>
2020-08-24 22:02:42 -0400

    most of app working

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts />
+      <Posts posts = {dummyData} />
       
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Post from './Post';
 import './Posts.css';
+import dummyData from '../../dummy-data';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+console.log(posts);
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */ }
-     
+     {posts.map(el => {return el.post})}
     
     
 
